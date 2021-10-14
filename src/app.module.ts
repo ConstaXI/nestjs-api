82ac0542c0469633ad5932161898c2cf995ff132
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-10-14 13:02:51 -0300

    feat: get users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.entity.ts
@@ -0,0 +1,26 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @Field()
+  id: string;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  surname: string;
+
+  @Column()
+  @Field()
+  email: string;
+
+  @Column()
+  @Field()
+  password: string;
+}
